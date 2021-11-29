2368a874699a93e32971d229ee3ebc58b1c11304
Zakaria Bidouli <zbidouli@e1r13p7.1337.ma>
2021-11-29 23:54:47 +0100

    lib

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,39 +1,87 @@
 #include "libft.h"
 
-int ft_atoi(const char *str)
-{
-    char *tmp;
-    int i;
-    int j;
-    int x;
+// int ft_atoi(const char *str)
+// {
+//     char *tmp;
+//     int i;
+//     int j;
+//    	unsigned int x;
 
-    tmp =  (char *) str;
-    i = 0;
-    j = 0;
-    while (tmp[i] == '-' && i < 1)
-    {
-        if (tmp[i] == '-')
-            j++;
+
+//     tmp =  (char *) str;
+	// if (!ft_strncmp(str, "-2147483648", 12))
+	// 	return (-2147483648);
+//     i = 0;
+//     j = 0;
+// 	if (tmp[i] == '+')
+// 		i++;
+//     else if (tmp[i] == '-' )
+//     {    
+// 		j++;
+// 		i++;
+// 	}
+// 	x = 0;
+//     while ( '0' <= tmp[i] && tmp[i] <= '9')
+// 	{
+// 		x *= 10;
+// 		x += tmp[i] - '0';
+// 		i++;
+// 	}
+// 	if (j != 0)
+// 		x = -x;
+// 	return (x);
+// }
+
+
+
+
+
+int	ft_atoi(const char	*str)
+{
+	int	i;
+	int	j;
+	int	x;
+	char *s;
+
+	i = 0;
+	j = 0;
+	s = (char *)str;
+	while (s[i] <= 32)
 		i++;
-    }
+	if (s[i] == '-')
+	{
+		i++;
+		j++;
+	}
+	else if (s[i] == '+')
+		i++;
+	if (!ft_strncmp(&str[i], "-2147483648", 12))
+		return (-2147483648);
 	x = 0;
-    while ( '0' <= tmp[i] && tmp[i] <= '9')
+	while ('0' <= s[i] && s[i] <= '9')
 	{
 		x *= 10;
-		x += tmp[i] - '0';
+		x += s[i] - '0';
 		i++;
 	}
-
 	if (j % 2)
 		x = -x;
 	return (x);
-    }
+}
 
+// #include <stdio.h>
+// #define TEST "+-42"
+
+// int	main()
+// {
+// 	printf("[%d]\n", ft_atoi(TEST));
+// 	printf("[%d]\n", atoi(TEST));
+
+// 	return (0);
+// }		 
 // #include <stdio.h>
 // int	main()
 // {
-// 	printf("[%d]\n",ft_atoi("--1297369"));
-// 	printf("[%d]",atoi("--1297"));
-
+// 	printf("%d",ft_atoi("---++  \n  \t \r \v \f 123abcd45"));
 // 	return (0);
 // }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,20 +1,21 @@
 #include "libft.h"
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    int i;
-    int j;
-    int k;
+    size_t i;
+    size_t j;
+    size_t k;
     char *tmp;
     char *tmp2;
+
     tmp = (char *)haystack;
     tmp2 = (char *)needle;
     j = 0;
     i = 0;
-    if (!*tmp2)
+	if (!*tmp2)
         return ((char *)tmp);
-    while (tmp[i] && i < len )
+    while (i < len && tmp[i])
     {
-        k = i;  
+        k = i;
         while ((tmp[k] == tmp2[j] || tmp2[j] == '\0') && (k < len))
         {
             if (tmp2 [j]== '\0')
@@ -29,14 +30,16 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
     return (0);
 }
 
+// #include <stdio.h>
+
 // int main ()
 // {
-//     const char *largestring = "TutorialsPoint";
-//     const char *smallstring = "u";
+//     const char *largestring = "aaabcabcd";
+//     const char *smallstring = "abcd";
 //     char *ptr;
 
-//     ptr = ft_strnstr(largestring, smallstring, 1);
+// 	ptr = ft_strnstr(largestring, smallstring, -9);
 //     printf ("%s\n", ptr);
-//     printf("%s\n",strnstr(largestring, smallstring, 1));
+//     printf("%s\n",strnstr(largestring, smallstring, 9));
 //     return 0;
 // }
